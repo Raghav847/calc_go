0305eda3ecd64b6005a3a74fad0524b7b9b48682
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-02-23 23:50:23 -0800

    noob operations

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var operation string
+	var num1, num2 int
+
+	fmt.Println("CALCULATOR GO 1.0")
+	fmt.Println("==================")
+	fmt.Println("SELECT OPERATION: (add, sub, mul, div)")
+	fmt.Scanf("%s", &operation)
+	fmt.Println("num1 : ")
+	fmt.Scanf("%d", &num1)
+	fmt.Println("num2 : ")
+	fmt.Scanf("%d", &num2)
+	switch operation {
+	case "add":
+		fmt.Println(num1 + num2)
+	case "sub":
+		fmt.Println(num1 - num2)
+	case "mul":
+		fmt.Println(num1 * num2)
+	case "div":
+		fmt.Println(num1 / num2)
+	}
 }
